e6d349f5dce6468b007d45b6ba1581905badd34b
wei <wei080781@gmail.com>
2025-10-27 08:09:35 +0800

    triangle

--- php/triangle.php
+++ php/triangle.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+    for($i=1 ; $i <= 5 ;$i++){
+
+    for($k=1; $k <=(5-$i);$k++){
+        echo "&nbsp;";    
+    }
     
+    for($j=1 ; $j <= (2*$i-1) ; $j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
 </body>
 </html>
